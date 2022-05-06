e41dd2e53167de90fb86518b512eec8d9334f3f4
Vinicius-SO <vhisoares39@gmail.com>
2022-05-05 23:52:37 -0300

    Dependency Reversion applied

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from 'nodemailer'
 import { prisma } from "./prisma";
+import { PrismaFeedbackRepository } from "./repositories/prisma/prisma-feedbacks-repositories";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-cases";
 
 export const routes = express.Router()
 
@@ -15,25 +17,28 @@ const transport = nodemailer.createTransport({
 
 routes.post('/feedbacks', async (req,res)=>{
     const { type, comment, screenshot } = req.body
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot
-        }
+    const prismaFeedbackRepository= new PrismaFeedbackRepository()
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbackRepository
+    )
+
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
     })
 
-    await transport.sendMail({
-        from: 'Equipe Feedget <oi.feedget@gmail.com>',
-        to: 'Vinicius Soares <vinicius.soares.so@gmail.com>',
-        subject: 'Novo Feedback',
-        html: [
-            `<div style="font-family:sans-serif; font-size: 16px; color:#111;">`,
-            `<p>Tipo do feedback: ${type}</p>`,
-            `<p>Comentário: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
-    })
+    // await transport.sendMail({
+    //     from: 'Equipe Feedget <oi.feedget@gmail.com>',
+    //     to: 'Vinicius Soares <vinicius.soares.so@gmail.com>',
+    //     subject: 'Novo Feedback',
+    //     html: [
+    //         `<div style="font-family:sans-serif; font-size: 16px; color:#111;">`,
+    //         `<p>Tipo do feedback: ${type}</p>`,
+    //         `<p>Comentário: ${comment}</p>`,
+    //         `</div>`
+    //     ].join('\n')
+    // })
 
-    return res.status(201).json({data: feedback})
+    return res.status(201).send()
 })
